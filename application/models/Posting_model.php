503769d4327d4c32ec94a4d66f8477a79cf4408d
DominicusRA <dominicusrakaalfryann@gmail.com>
2023-05-09 13:41:42 +0700

    step 1 posting piutang

--- application/models/Posting_model.php
+++ application/models/Posting_model.php
@@ -3,11 +3,58 @@ class Posting_model extends CI_MODEL
 {
     function posting($data)
     {
-        print_r($data);
-        if ($this->db->query('CREATE TEMPORARY TABLE t_dpiutang (refno character varying (15), bayar numeric, tf date, bulan numeric, tahun numeric)')) {
-            return true;
+        // print_r($data);
+        if ($data['range']['bulan'] - 1 == 0) {
+            $periode_sebelumnya = $data['range']['tahun'] - 1 . 12;
         } else {
-            return false;
+            $periode_sebelumnya = $data['range']['tahun'] . str_pad($data['range']['bulan'] - 1, 2, 0, STR_PAD_LEFT);
+        }
+        $periode = $data['periode'];
+
+        echo $periode_sebelumnya;
+        // cek apakah sudah ada yang di posting
+        // jika ada data yang bersangkutan dengan periode posting di hapus
+        $this->db->select();
+        $this->db->from('piut');
+        $this->db->where('periode', $data['periode']);
+        $count_periode = $this->db->count_all_results();
+        // print_r($count_periode);
+
+        if ($count_periode > 0) {
+            echo "ini berjalan";
+            if ($this->db->delete('piut', array('periode' => $data['periode']))) {
+                echo "hapus data berhasil";
+            }
+        } else {
+            // echo "kzsjdfn";
+        }
+
+        //ambil hasil postingan periode lalu, kemudian ubang jadi periode yang di minta STEP 1
+
+        $query_t_piut = $this->db->query('create temporary table t_piut(
+            periode character varying,
+            nilai numeric,
+            nojnl character varying
+            )');
+
+        echo $query_t_piut;
+        //jika selesai membuat temporary table, maka di eksekusi query STEP 1
+        if ($query_t_piut) {
+            $insert_t_piut = $this->db->query("INSERT INTO t_piut (periode, nilai, nojnl) 
+            SELECT (CASE WHEN periode = '" . $periode_sebelumnya . "' then '" . $periode . "' end) AS periode, (nilai-bayar) as nilai, nojnl
+            from piut
+            where periode like '%" . $periode_sebelumnya . "%'
+            and (nilai - bayar) > 0");
+
+
+            if ($insert_t_piut) {
+                $this->db->select('*');
+                $this->db->from('t_piut');
+                $result_step_1 = $this->db->get();
+                echo "<pre>";
+                print_r($result_step_1->result_array());
+                echo "</pre>";
+            }
         }
     }
 }
