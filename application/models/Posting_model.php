9103584eeb2e1c4ae47098268e13e9ff74a49789
DominicusRA <dominicusrakaalfryann@gmail.com>
2023-05-12 16:45:07 +0700

    posting piutang

--- application/models/Posting_model.php
+++ application/models/Posting_model.php
@@ -22,9 +22,9 @@ class Posting_model extends CI_MODEL
 
         if ($count_periode > 0) {
             echo "ini berjalan";
-            // if ($this->db->delete('piut', array('periode' => $data['periode']))) {
-            //     echo "hapus data berhasil";
-            // }
+            if ($this->db->delete('piut', array('periode' => $data['periode']))) {
+                echo "hapus data berhasil";
+            }
         } else {
             // echo "kzsjdfn";
         }
@@ -37,7 +37,14 @@ class Posting_model extends CI_MODEL
             periode character varying,
             nilai numeric,
             nojnl character varying,
-            ket character varying
+            ket character varying,
+            kode character varying,
+            tgl date,
+            kodesls character varying,
+            acc character varying,
+            tjt date,
+            type character varying, 
+            fgroup character varying
             )');
         $query_t_dpiutang = $this->db->query('create temporary table t_dpiutang(
             refno character varying,
@@ -47,25 +54,25 @@ class Posting_model extends CI_MODEL
         //jika selesai membuat temporary table, maka di eksekusi query STEP 1
         if ($query_t_piut) {
 
-            $insert_t_piut = $this->db->query("INSERT INTO t_piut (periode, ket, nilai, nojnl) 
-            SELECT (CASE WHEN periode = '" . $periode_sebelumnya . "' then '" . $periode . "' end) AS periode, (CASE WHEN periode = '" . $periode_sebelumnya . "' then 'piut' end) AS ket, (nilai-bayar) as nilai, nojnl
+            $insert_t_piut = $this->db->query("INSERT INTO t_piut (acc, kode, tgl, tjt,type,kodesls,periode, nilai, nojnl) 
+            SELECT acc, kode, tgl, tjt,type,kodesls,(CASE WHEN periode = '" . $periode_sebelumnya . "' then '" . $periode . "' end) AS periode, (nilai-bayar) as nilai, nojnl
             from piut
             where periode like '%" . $periode_sebelumnya . "%'
             and (nilai - bayar) != 0
-            group by nojnl, periode, nilai, bayar");
+            group by acc,kode,tgl,tjt,type,kodesls,nojnl, periode, nilai, bayar");
 
             // STEP 2
             // ambil data baru dari minv
-            $insert_t_piutang_minv = $this->db->query("INSERT INTO t_piut (ket, periode, nilai, nojnl)
-            SELECT (CASE WHEN extract(YEAR FROM tf) = '2022' then 'minv' end) AS ket, concat(extract(YEAR FROM tf),extract(MONTH FROM tf)) AS periode, total as nilai , nf AS nojnl from minv where extract(YEAR FROM tf) = " . $data['range']['tahun'] . " and extract(MONTH FROM tf) =  " . $data['range']['bulan'] . "");
+            $insert_t_piutang_minv = $this->db->query("INSERT INTO t_piut (acc, type, periode, nilai, nojnl, kode, tgl, tjt, kodesls )
+            SELECT (CASE WHEN 0 = 0 then '114.100' end) AS acc, (CASE WHEN 0 = 0 then 'FAKTUR' end) AS type, concat(extract(YEAR FROM tf),to_char(tf,'MM')) AS periode, total as nilai , nf AS nojnl , kode, tf as tgl, tjt, slskode from minv where extract(YEAR FROM tf) = " . $data['range']['tahun'] . " and extract(MONTH FROM tf) =  " . $data['range']['bulan'] . "");
             // STEP 3
             //ambil dp di tkpiut
-            $insert_t_piutang_tkpiut = $this->db->query("INSERT INTO t_piut (ket, periode, nilai, nojnl)
-            SELECT (CASE WHEN extract(YEAR FROM tgl) = '2022' then 'tkpiut' end) AS ket, concat(extract(YEAR FROM tgl),extract(MONTH FROM tgl)) AS periode, qty AS nilai, nojnl FROM tkpiut WHERE  extract(YEAR FROM tgl) = " . $data['range']['tahun'] . " and extract(MONTH FROM tgl) =  " . $data['range']['bulan'] . "");
+            $insert_t_piutang_tkpiut = $this->db->query("INSERT INTO t_piut (acc, type, periode, nilai, nojnl, kode, tgl, tjt )
+            SELECT (CASE WHEN 0 = 0 then '215.210' end) AS acc, (CASE WHEN 0 = 0 then 'KOREKSI' end) AS type,concat(extract(YEAR FROM tgl),to_char(tgl,'MM')) AS periode, qty AS nilai, nojnl , kode, tgl, tgl as tjt FROM tkpiut WHERE  extract(YEAR FROM tgl) = " . $data['range']['tahun'] . " and extract(MONTH FROM tgl) =  " . $data['range']['bulan'] . "");
             // STEP 4
             // ambil returan di mrj
-            $insert_t_piutang_mrj = $this->db->query("INSERT INTO t_piut (ket, periode, nilai, nojnl)
-            SELECT (CASE WHEN extract(YEAR FROM tf) = '2022' then 'mrj' end) as ket, concat(extract(YEAR FROM tf),extract(MONTH FROM tf)) AS periode, total AS nilai, nf AS nojnl from mrj where extract(YEAR FROM tf) = " . $data['range']['tahun'] . " and extract(MONTH FROM tf) =  " . $data['range']['bulan'] . "");
+            $insert_t_piutang_mrj = $this->db->query("INSERT INTO t_piut (acc, type,periode, nilai, nojnl, kode, tgl, tjt)
+            SELECT (CASE WHEN 0 = 0 then '114.100' end) AS acc, (CASE WHEN 0 = 0 then 'RETUR' end) AS type, concat(extract(YEAR FROM tf),to_char(tf,'MM')) AS periode, total AS nilai, nf AS nojnl, kode, tf as tgl , tjt from mrj where extract(YEAR FROM tf) = " . $data['range']['tahun'] . " and extract(MONTH FROM tf) =  " . $data['range']['bulan'] . "");
             // STEP 5
             //ambil pembayaran di dpiutang masukan ke twmporary table t_dpiutang
 
@@ -126,12 +133,17 @@ class Posting_model extends CI_MODEL
 
 
 
-        $this->db->select('nojnl, nilai, (CASE WHEN bayar is NULL THEN 0 ELSE bayar END) AS bayar, periode');
+        $this->db->select('nojnl, acc, kode, tgl, tjt, type, nilai, (CASE WHEN bayar is NULL THEN 0 ELSE bayar END) AS bayar, ket, fgroup, kodesls, periode');
         $this->db->from('t_dpiutang');
         $this->db->join('t_piut', 't_piut.nojnl = t_dpiutang.refno', 'right');
         $result_t_dpiutang = $this->db->get();
         echo "<pre>";
         print_r($result_t_dpiutang->result_array());
         echo "</pre>";
+
+        $insert_piut = $this->db->query("INSERT INTO piut (nojnl, acc, kode, tgl, tjt, type, nilai, bayar, ket, fgroup, kodesls, periode)
+            SELECT nojnl, acc, kode, tgl, tjt, type, nilai, (CASE WHEN bayar is NULL THEN 0 ELSE bayar END) AS bayar, ket, fgroup, kodesls, periode 
+            from t_dpiutang
+            RIGHT JOIN t_piut on t_piut.nojnl = t_dpiutang.refno");
     }
 }
